016a49582188c9292b07a9e6034569830ed6d5a7
Soumya1743 <soumyarangaihagari043@gmail.com>
2024-07-19 16:16:37 +0530

    Matrix Addition

--- Arrays.java
+++ Arrays.java
@@ -1,8 +1,10 @@
+package Arrays_JAVA;
+
 import java.util.*;
 public class Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("How many elements are there in your array: ");
+        System.out.print("How many elements are there in your array: ");
         int size = sc.nextInt();
         int arr[] = new int[size];
         System.out.println("Enter " + size + " Elements: ");
--- Matrix_Addition.java
+++ Matrix_Addition.java
@@ -1,30 +1,36 @@
+package Arrays_JAVA;
+
 public class Matrix_Addition {
     public static void main(String[] args) {
-        int arr1[][] = {{1,2},{3,4}};
+        int array1[][] = {{1,2},{3,4}};
+        int array2[][] = {{5,6},{7,8}};
         for(int i=0; i<2; i++){
             for(int j=0; j<2; j++){
-                System.out.print(arr1[i][j] + "\t");
+                System.out.print(array1[i][j] + "\t");
             }
             System.out.println();
         }
 
-        int arr2[][] = {{1,2},{3,4}};
         for(int i=0; i<2; i++){
             for(int j=0; j<2; j++){
-                System.out.print(arr2[i][j] + " \t");
+                System.out.print(array2[i][j] + " \t");
             }
             System.out.println();
         }
         System.out.println();
 
-
-        int addition[][]=new int[2][2];
-        for(int i=0; i<2; i++){
-            for(int j=0; j<2; j++){
-                addition[i][j] = arr1[i][j] + arr2[i][j];
-                System.out.print(addition[i][j] + "\t");
+        if(array1.length != array2.length){
+            System.out.println("Lengths are not equal, can't do Addition!");
+        }
+        else{
+            int addition[][]=new int[2][2];
+            for(int i=0; i<2; i++){
+                for(int j=0; j<2; j++){
+                    addition[i][j] = array1[i][j] + array2[i][j];
+                    System.out.print(addition[i][j] + "\t");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
