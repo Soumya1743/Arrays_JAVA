2a07602a8232f06699b2a19dd89165b691ac7387
Soumya1743 <soumyarangaihagari043@gmail.com>
2024-07-20 00:20:32 +0530

    Arrays in JAVA

--- Matrix_Addition.java
+++ Matrix_Addition.java
@@ -1,36 +1,49 @@
 package Arrays_JAVA;
-
+import java.util.*;
 public class Matrix_Addition {
     public static void main(String[] args) {
-        int array1[][] = {{1,2},{3,4}};
-        int array2[][] = {{5,6},{7,8}};
-        for(int i=0; i<2; i++){
-            for(int j=0; j<2; j++){
-                System.out.print(array1[i][j] + "\t");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int row = sc.nextInt();
+        System.out.print("Enter number of Columns: ");
+        int col = sc.nextInt();
+        int array1[][] = new int[row][col];
+        System.out.print("Enter the elements of array1: ");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                array1[i][j] = sc.nextInt();
+            }
+        }
+
+        int array2[][] = new int[row][col];
+        System.out.print("Enter the elements of array2: ");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                array2[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                System.out.print(array1[i][j]+"\t");
             }
             System.out.println();
         }
 
-        for(int i=0; i<2; i++){
-            for(int j=0; j<2; j++){
-                System.out.print(array2[i][j] + " \t");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                System.out.print(array2[i][j]+"\t");
             }
             System.out.println();
         }
-        System.out.println();
 
-        if(array1.length != array2.length){
-            System.out.println("Lengths are not equal, can't do Addition!");
-        }
-        else{
-            int addition[][]=new int[2][2];
-            for(int i=0; i<2; i++){
-                for(int j=0; j<2; j++){
-                    addition[i][j] = array1[i][j] + array2[i][j];
-                    System.out.print(addition[i][j] + "\t");
-                }
-                System.out.println();
+        int addition[][] = new int[2][2];
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                addition[i][j] = array1[i][j] + array2[i][j];
+                System.out.print(addition[i][j]+"\t");
             }
+            System.out.println();
         }
     }
 }
